9e7c46650738f316bc0f0ce9a90a55a0c4ccf912
Francielle Limonge <152073275+naagaii@users.noreply.github.com>
2024-04-26 09:51:11 -0300

    correções

--- IntroJS/main.js/atm.js
+++ IntroJS/main.js/atm.js
@@ -1,37 +1,44 @@
 let saldo = 0.00;
 
 const saldoSpan = document.querySelector("#saldo-atual");
-const valorInput = document.querySelector("#saldo-atual");
-const formatadorMoedaReal = new Intl.NumberFormat('pt-BR' {
+const valorInput = document.querySelector("#valor-monetario");
+const formatadorMoedaReal = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
-    currency: 'BRL'});
+    currency: 'BRL'
+});
 
-    function sacar(){
-        
-    }
-
-    function depositar(){
-        let valorDeposito = +valorInput.value.trim();
-        let confirmacao = confirm("Você deseja depositar? " + formatadorMoedaReal.format(valorDeposito) + "?");
-        if (confirmacao){
-            saldo+= valorDeposito;
+function sacar(){
+    let valorSaque = parseFloat(valorInput.value.trim());
+    if (valorSaque > saldo) {
+        alert("Saldo insuficiente.");
+    } else {
+        let confirmacao = confirm("Você deseja sacar " + formatadorMoedaReal.format(valorSaque) + "?");
+        if (confirmacao) {
+            saldo -= valorSaque;
             atualizarSaldo(saldo);
-            alert("Depósito de " + formatadorMoedaReal.format(valorDeposito) + "realizado com sucesso.");
+            alert("Saque de " + formatadorMoedaReal.format(valorSaque) + " realizado com sucesso.");
+        } else {
+            alert("Saque cancelado pelo usuário.");
         }
-        else{
-            alert("Deposito cancelado pelo usuário. ");
-
-        }
-
     }
+}
 
-    function atualizarSaldo(valor){
-        saldoSpan.textContent = formatadorMoedaReal
-        .format(valor);
-
-    }
-
-    window.addEventListener("load", () => {
+function depositar(){
+    let valorDeposito = parseFloat(valorInput.value.trim());
+    let confirmacao = confirm("Você deseja depositar " + formatadorMoedaReal.format(valorDeposito) + "?");
+    if (confirmacao) {
+        saldo += valorDeposito;
         atualizarSaldo(saldo);
+        alert("Depósito de " + formatadorMoedaReal.format(valorDeposito) + " realizado com sucesso.");
+    } else {
+        alert("Depósito cancelado pelo usuário.");
+    }
+}
 
-    });
+function atualizarSaldo(valor){
+    saldoSpan.textContent = formatadorMoedaReal.format(valor);
+}
+
+window.addEventListener("load", () => {
+    atualizarSaldo(saldo);
+});
--- IntroJS/main.js/par_impar.js
+++ IntroJS/main.js/par_impar.js
@@ -1,24 +1,21 @@
 const resultadoP = document.querySelector("#resultado");
-const nuemroInput = document.querySelector("#numero")
+const numeroInput = document.querySelector("#numero");
 
 function ePar(numero){
-    return (numero%2) ==0;
+    return (numero % 2) === 0;
 }
 
-//procedimento
 function verificarParOuImpar(){
     let numero = +numeroInput.value;
-}
-if (isNaN(numero)){
-    resultadoP.textcontent = "Por favor, insira um número.";
-    return;
-}
+    
+    if (isNaN(numero)){
+        resultadoP.textContent = "Por favor, insira um número.";
+        return;
+    }
 
-
-if (ePar(numero)){
-    resultadoP.textContent = numero + " é par. ";
-}
-
-else {
-    resultadoP.textContent = numero + " é ímpar. ";
+    if (ePar(numero)){
+        resultadoP.textContent = numero + " é par.";
+    } else {
+        resultadoP.textContent = numero + " é ímpar.";
+    }
 }
